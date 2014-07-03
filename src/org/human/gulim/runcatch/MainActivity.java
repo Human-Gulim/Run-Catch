860b5cc744e1a7a73ee17bf989411884f130530a
Mnkai <minori@mnetwork.co.kr>
2014-07-03 21:09:15 +0900

    메인 화면 Activity - 미완
    TODO: 각각의 Activity (방 생성, 방 참여) 로 연결 - intent

--- src/org/human/gulim/runcatch/MainActivity.java
+++ src/org/human/gulim/runcatch/MainActivity.java
@@ -2,8 +2,7 @@ package org.human.gulim.runcatch;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +10,17 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+	
+	public void makeRoom (View v)
+	{
+		// 방을 만드는 Activity 실행
+		finish();
+	}
+	
+	public void joinRoom (View v)
+	{
+		// 방에 참가하는 Activity 실행
+		finish();
 	}
 }
